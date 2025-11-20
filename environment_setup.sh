3b8c978e66ee2216e4177fb43479e7987c36e08d
jconwell3115 <jconwell3115@gmail.com>
2025-11-20 15:17:08 -0500

    Update repository URLs to use GitHub

--- environment_setup.sh
+++ environment_setup.sh
@@ -56,7 +56,7 @@ fi
 if [[ -z "$REPO_NAME" ]]; then
   warn "No repo name provided - setting up project directory only (no repository will be cloned)"
 else
-  read -rp "Enter the repo owner: (Leave blank for Network-DevOps) " REPO_OWNER
+  read -rp "Enter the repo owner: (Leave blank for jconwell3115) " REPO_OWNER
 fi
 
 # Validate system requirements
@@ -158,7 +158,7 @@ fi
 # ------------- Clone Work Tools Repos -------------
 section "Cloning work tools repositories..."
 
-clone_or_pull "git@git.marriott.com:jconw356/global_env.git"
+clone_or_pull "git@github.com:jconwell3115/global_env.git"
 
 # Setup global_env directory with pre-commit
 cd "$GLOBAL_ENV_DIR" || exit
@@ -175,7 +175,7 @@ cd "$WORK_TOOLS_DIR" || exit
 info "Pausing for 30 seconds or until you press enter ..."
 read -t 30 -rp "" || true
 
-clone_or_pull "git@git.marriott.com:jconw356/bin.git"
+clone_or_pull "git@github.com:jconwell3115/bin.git"
 
 # Setup bin directory with config files and pre-commit
 cd "$BIN_DIR" || exit
@@ -269,7 +269,7 @@ else
   # Clone project repo (only if REPO_NAME is provided)
   if [[ -n "$REPO_NAME" ]]; then
     info "Cloning project repository..."
-    clone_or_pull "git@git.marriott.com:${REPO_OWNER:-Network-DevOps}/$REPO_NAME.git"
+    clone_or_pull "git@github.com:${REPO_OWNER:-jconwell3115}/$REPO_NAME.git"
 
     cd "$PROJECT_DIR/$REPO_NAME" || exit
     info "Changed to repository directory: $(pwd)"
